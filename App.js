d8ac75e54135da045bc4447f0254014da0e9add1
lightfootc <cody.c.lightfoot@gmail.com>
2021-12-16 23:19:09 -0600

    Initial app build. Created first component for HeaderTabs.

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Text, View } from 'react-native';
+import Home from "./screens/Home";
+
+export default function App() {
+  return <Home />;
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Beauty Run Mobile App
