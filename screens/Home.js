f82d341fb8e1c8c2f02bd0ef2d55cba7e8e389de
lightfootc <cody.c.lightfoot@gmail.com>
2021-12-17 17:40:22 -0600

    added the store items component. hard code in store data.

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import Categories from '../components/Categories';
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
+import StoreItems, { localSalons } from '../components/StoreItems';
 
 export default function home() {
+    const [salonData, setSalonData] = React.useState(localSalons)
     return (
         <SafeAreaView style = {{ backgroundColor: "#eee", flex: 1 }}>
             <View style = {{ backgroundColor: "white", padding: 15 }}>
                 <HeaderTabs />
                 <SearchBar />
             </View>
-            <Categories />
+            <ScrollView showsVerticalScrollIndicator={ false }>
+                <Categories />
+                <StoreItems salonData={salonData} />
+            </ScrollView>
         </SafeAreaView>
     );
 }
